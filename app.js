8c79df53e60e07bc85cbdc61cd0d3ced6a74f35a
codingwolf-at <atameshwari9@gmail.com>
2021-09-16 20:02:41 +0530

    FEAT: input validation

--- app.js
+++ app.js
@@ -8,20 +8,24 @@ const calculateProfitAndLoss = () => {
   const initialPrice = Number(initialPriceInput.value);
   const stockNumber = Number(stockNumberInput.value);
   const currentPrice = Number(currentPriceInput.value);
-  
-  if (initialPrice < currentPrice) {
-    const profit = (currentPrice - initialPrice) * stockNumber;
-    const profitPercentage = (profit / (initialPrice * stockNumber)) * 100;
-    outputDiv.style.color = "green";
-    outputDiv.innerText = `Yay!! Your Profit is ${profit.toFixed(2)} and profit Percentage is ${profitPercentage.toFixed(2)}%`;
-  } else if (currentPrice < initialPrice) {
-    const loss = (initialPrice - currentPrice) * stockNumber;
-    const lossPercentage = (loss / (initialPrice * stockNumber)) * 100;
-		outputDiv.style.color = 'red';
-		outputDiv.innerText = `Whoops!! Your loss is ${loss.toFixed(2)} and loss Percentage is ${lossPercentage.toFixed(2)}% :(`;
+
+  if (initialPrice && stockNumber && currentPrice) {
+    if (initialPrice < currentPrice) {
+      const profit = (currentPrice - initialPrice) * stockNumber;
+      const profitPercentage = (profit / (initialPrice * stockNumber)) * 100;
+      outputDiv.style.color = "green";
+      outputDiv.innerText = `Yay!! Your Profit is ${profit.toFixed(2)} and profit Percentage is ${profitPercentage.toFixed(2)}%`;
+    } else if (currentPrice < initialPrice) {
+      const loss = (initialPrice - currentPrice) * stockNumber;
+      const lossPercentage = (loss / (initialPrice * stockNumber)) * 100;
+      outputDiv.style.color = 'red';
+      outputDiv.innerText = `Whoops!! Your loss is ${loss.toFixed(2)} and loss Percentage is ${lossPercentage.toFixed(2)}% :(`;
+    } else {
+      outputDiv.style.color = 'white';
+      outputDiv.innerHTML = `No pain No gain, and no gain no pain :)`;
+    }
   } else {
-    outputDiv.style.color = 'white';
-		outputDiv.innerHTML = `No pain No gain, and no gain no pain :)`;
+    alert("Fill all the fields correctly \nInput can not be 0 or empty!")
   }
 }
 
