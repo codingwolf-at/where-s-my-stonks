248274c4525a9bc2ddc47391076b5766470f8325
codingwolf-at <atameshwari9@gmail.com>
2021-09-16 19:37:26 +0530

    FINISHED: logic is working with html but no styling

---
+++ app.js
@@ -0,0 +1,28 @@
+const initialPriceInput = document.querySelector("#initial-price-input");
+const stockNumberInput = document.querySelector("#stock-number-input");
+const currentPriceInput = document.querySelector("#current-price-input");
+const tellMeBtn = document.querySelector("#tell-me-btn");
+const outputDiv = document.querySelector("#output-div");
+
+const calculateProfitAndLoss = () => {
+  const initialPrice = Number(initialPriceInput.value);
+  const stockNumber = Number(stockNumberInput.value);
+  const currentPrice = Number(currentPriceInput.value);
+  
+  if (initialPrice < currentPrice) {
+    const profit = (currentPrice - initialPrice) * stockNumber;
+    const profitPercentage = (profit / (initialPrice * stockNumber)) * 100;
+    outputDiv.style.color = "green";
+    outputDiv.innerText = `Yay!! Your Profit is ${profit.toFixed(1)} and profit Percentage is ${profitPercentage.toFixed(1)}%`;
+  } else if (currentPrice < initialPrice) {
+    const loss = (initialPrice - currentPrice) * stockNumber;
+    const lossPercentage = (loss / (initialPrice * stockNumber)) * 100;
+		outputDiv.style.color = 'red';
+		outputDiv.innerText = `Whoops!! Your loss is ${loss.toFixed(1)} and loss Percentage is ${lossPercentage.toFixed(1)}% :(`;
+  } else {
+    outputDiv.style.color = 'white';
+		outputDiv.innerHTML = `No pain No gain, and no gain no pain :)`;
+  }
+}
+
+tellMeBtn.addEventListener("click", calculateProfitAndLoss)
