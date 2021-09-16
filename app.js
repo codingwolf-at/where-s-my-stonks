5949548349fb79d2a5320aa4f6f01f7746a7d9c5
codingwolf-at <atameshwari9@gmail.com>
2021-09-16 19:51:44 +0530

    FIX: updating decimal points to 2 in .toFixed()

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ const calculateProfitAndLoss = () => {
     const profit = (currentPrice - initialPrice) * stockNumber;
     const profitPercentage = (profit / (initialPrice * stockNumber)) * 100;
     outputDiv.style.color = "green";
-    outputDiv.innerText = `Yay!! Your Profit is ${profit.toFixed(1)} and profit Percentage is ${profitPercentage.toFixed(1)}%`;
+    outputDiv.innerText = `Yay!! Your Profit is ${profit.toFixed(2)} and profit Percentage is ${profitPercentage.toFixed(2)}%`;
   } else if (currentPrice < initialPrice) {
     const loss = (initialPrice - currentPrice) * stockNumber;
     const lossPercentage = (loss / (initialPrice * stockNumber)) * 100;
 		outputDiv.style.color = 'red';
-		outputDiv.innerText = `Whoops!! Your loss is ${loss.toFixed(1)} and loss Percentage is ${lossPercentage.toFixed(1)}% :(`;
+		outputDiv.innerText = `Whoops!! Your loss is ${loss.toFixed(2)} and loss Percentage is ${lossPercentage.toFixed(2)}% :(`;
   } else {
     outputDiv.style.color = 'white';
 		outputDiv.innerHTML = `No pain No gain, and no gain no pain :)`;
